7e7fdb116fe06a7ce60780824ba99854025a7578
JulianGomez <julian.gomez20@udea.edu.co>
2023-09-23 15:18:36 -0500

    Ejercicios 2 y 3, depuracíon de funciones

--- QTCODE/main.cpp
+++ QTCODE/main.cpp
@@ -1,5 +1,4 @@
 
-
 #include <iostream>
 
 
@@ -11,6 +10,10 @@ const int columnas = 8, filas = 8;
 void verificacion(int **matriz);
 
 void patron1(int **matriz);
+
+void patron2(int **matriz);
+
+void patron3(int **matriz);
 
 int main()
 {
@@ -38,7 +41,17 @@ cout<<endl;
 verificacion(matriz);
 
 cout<<endl;
+patron2(matriz);
+
+cout<<endl;
+cout<<"Aquí patron 1"<<endl;
 patron1(matriz);
+
+cout<<endl;
+cout<<"Aquí patron 3"<<endl;
+patron3(matriz);
+
+
 
 
     return 0;
@@ -71,7 +84,7 @@ cout<<"["<<matriz[i][j]<<"]";
 
 }
 
-void patron1(int **matriz)
+void patron2(int **matriz)
 {
   for(int i = 0;i<filas;i++)
 {
@@ -96,4 +109,53 @@ for(int j = 0; j<columnas;j++)
 cout<<"["<<matriz[i][j]<<"]";
 }
 }
+}
+
+void patron1(int **matriz){
+        for (int i = 0; i < filas; ++i) {
+            for (int j = 0; j < columnas; ++j) {
+                matriz[i][j] = 0;
+            }
+        }
+
+for (int i = 0; i < 4; ++i) {
+for (int j = 3 - i; j <= 4 + i; ++j) {
+matriz[i][j] = 1;
+matriz[7 - i][j] = 1;
+}
+}
+ for (int i = 0; i < filas; ++i) {
+for (int j = 0; j < columnas; ++j) {
+cout << "[" << matriz[i][j] << "]";
+}
+cout <<endl; // Cambiar de línea al final de cada fila
+}
+}
+
+void patron3(int **matriz){
+    for (int fila = 0; fila < 8; fila++) {
+        for (int col = 0; col < 8; col++) {
+            int patron = (fila / 2) % 4;
+            if (patron == 0 || patron == 2) {
+                if ((col == 0) || (col == 1) || (col == 3) || (col == 4) || (col == 6) || (col == 7)) {
+                    matriz[fila][col] = 1;
+                } else {
+                    matriz[fila][col] = 0;
+                }
+            } else {
+                if ((col == 1) || (col == 2) || (col == 4) || (col == 5) || (col == 7)) {
+                    matriz[fila][col] = 1;
+                } else {
+                    matriz[fila][col] = 0;
+                }
+            }
+        }
+    }
+
+for (int fila = 0; fila < 8; fila++) {
+        for (int col = 0; col < 8; col++) {
+            cout << matriz[fila][col] << " ";
+        }
+        cout<<endl;
+    }
 }
